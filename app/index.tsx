f411180756282d29615e272e48401cb1579d7e42
Your Name <rindepixels@gmail.com>
2024-11-26 01:15:07 +0100

    Added onboarding screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,81 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyle}>Aora</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
-        Go to Profile
-      </Link>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View style={styles.contentBox}>
+          <Image style={styles.logoStyle} source={images.logo} />
+          <Image style={styles.cardStyle} source={images.cards} />
+          <View style={styles.textContainer}>
+            <Text style={styles.textStyle}>
+              Discover Endless Possibilities with{" "}
+              <Text style={styles.inlineStyle}>Aora</Text>
+            </Text>
+            <Image style={styles.pathStyles} source={images.path} />
+          </View>
+          <Text style={styles.taglineStyles}>
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    backgroundColor: "#161622",
+  },
+  contentBox: {
+    width: "100%",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
+    minHeight: "100%",
+    paddingVertical: 16,
+  },
+  logoStyle: {
+    width: 130,
+    height: 84,
+    resizeMode: "contain",
+  },
+  cardStyle: {
+    maxWidth: 380,
+    width: "100%",
+    height: 84,
+    resizeMode: "contain",
+  },
+  textContainer: {
+    position: "relative",
+    marginTop: 20,
   },
   textStyle: {
-    fontSize: 18,
-    fontFamily: "Poppins-Black",
+    fontSize: 30,
+    color: "white",
+    textAlign: "center",
+    fontWeight: 700,
+  },
+  inlineStyle: {
+    color: "#FF8E01",
+  },
+  pathStyles: {
+    width: 136,
+    height: 15,
+    position: "absolute",
+    bottom: -8,
+    right: -32,
+    resizeMode: "contain",
+  },
+  taglineStyles: {
+    fontSize: 14,
+    fontFamily: "Poppins-Regular",
+    color: "#f7fafc",
+    marginTop: 28,
+    justifyContent: "center",
   },
 });
---
+++ components/CustomBotton.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CustomBotton = () => {
+  return (
+    <View>
+      <Text>CustomBotton</Text>
+    </View>
+  );
+};
+
+export default CustomBotton;
+
+const styles = StyleSheet.create({});
